f7d4c8a3847c0aaa9b697c78797b64c193714c5e
Yuri Tokio <yuri@sheild.com.br>
2023-09-20 05:11:25 -0300

    Rotas e Controllers do Produto finalizado

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -21,7 +21,77 @@ const produtoController = {
         } catch (error){
             console.log(error)
         }
-    }
+    },
+    getAll: async (req, res) => {
+        try{
+            const produtos = await ProdutoModel.find();
+
+            res.json(produtos);
+        } catch (error){
+            console.log(error);
+        }
+    },
+    get: async(req, res) => {
+        try {
+            // id => req === URL
+            const id = req.params.id
+
+            const produto = await ProdutoModel.findById(id)
+            
+            if (!produto){
+                res.json(404).json({ msg: "Produto não encontrado."})
+                return;
+            }
+
+            res.json(produto)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    delete: async(req, res) => {
+        try {
+            const id = req.params.id
+
+            const produto = await ProdutoModel.findById(id)
+            
+            if (!produto){
+                res.json(404).json({ msg: "Produto não encontrado."})
+                return;
+            }
+
+            const deletedProduto = await ProdutoModel.findByIdAndDelete(id)
+
+            res.status(200).json({deletedProduto, msg: "Produto excluído com sucesso!"})
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    update: async(req, res) => {
+        try {
+            const id = req.params.id
+
+            const produto = {
+                nm_produto: req.body.nm_produto,
+                ds_produto: req.body.ds_produto,
+                valor: req.body.valor,
+                categoria: req.body.categoria,
+                fornecedor: req.body.fornecedor,
+                url_img: req.body.url_img,
+                qtd: req.body.qtd
+            };
+
+            const updateProduto = await ProdutoModel.findByIdAndUpdate(id, produto)
+            
+            if (!updateProduto){
+                res.json(404).json({ msg: "Produto não encontrado."})
+                return;
+            }
+
+            res.status(200).json({produto, msg: "Produto atualizado com sucesso!"})
+        } catch (error) {
+            console.log(error)
+        }
+    },
 };
 
 // exportando controller para usar nas rotas
