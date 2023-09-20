336aaffd21f28c8490ac444c94b4e933fdaa828a
Yuri Tokio <yuri@sheild.com.br>
2023-09-20 01:53:03 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// importando o express e o modulo cors
+const express = require("express")
+const cors = require("cors")
+
+// invocando express para centralizar todas configurações da aplicação
+const app = express()
+
+// aplicação está usando cors
+app.use(cors())
+
+// middlers - irá permitir comunicação de dados via Json
+app.use(express.json())
+
+app.listen(4000, function(){
+    console.log("Servidor está online na porta 4000!")
+})
